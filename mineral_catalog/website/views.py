f41790c979d53ad272d095de50c82de4b2d3712c
Hyungmo Gu <guhyungm7@gmail.com>
2019-06-08 13:49:42 -0700

    feat: Make home page to fetch and load data

--- mineral_catalog/website/views.py
+++ mineral_catalog/website/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 
+from .models import Mineral
 
 # Create your views here.
+
+
 def home_page(request):
-    return render(request, 'website/home_page.html', {'minerals': []})
+    # import all items from database
+    minerals = Mineral.objects.all()
+
+    # load items to view
+    return render(request, 'website/home_page.html', {'minerals': minerals})
 
 
 def mineral_detail(request, mineral_pk):
